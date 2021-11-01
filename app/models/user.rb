4b7e08ac510576fa2c3ac9bd8eaa9ea50df439c1
thanghd-1196 <ho.duc.thang@sun-asterisk.com>
2021-11-01 13:01:55 +0700

    Chapter 11: Account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,18 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save{self.email = email.downcase}
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: Settings.atrr.lenght_50}
   validates :email, presence: true, length: {maximum: Settings.atrr.lenght_255},
   format: {with: Settings.regex.email},
   uniqueness: true
   has_secure_password
-  validates :password_digest, presence: true, length: {minimum: Settings.atrr.lenght_6}
-
+  validates :password_digest, presence: true, length: {minimum: Settings.atrr.lenght_6}, allow_nil: true
   def downcase_email
-    self.email.downcase!
+    self.email = email.downcase
   end
-
   class << self
-    def digest(string)
+    def digest string
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
       BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
@@ -29,11 +28,27 @@ class User < ApplicationRecord
     update_attribute :remember_digest, User.digest(remember_token)
   end
 
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 end
