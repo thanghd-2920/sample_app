4792915a3f226b93ba6ac5d9b78367914b29ac83
thanghd-1196 <ho.duc.thang@sun-asterisk.com>
2021-11-01 13:01:55 +0700

    Chapter 9:Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] ? remember(user) : forget(user)
       flash[:success] = t"new.title"
       redirect_to user
     else
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
