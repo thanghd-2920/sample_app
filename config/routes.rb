0c24cf5344d6922e5c9c1e803c0075a29fd6e46b
thanghd-1196 <ho.duc.thang@sun-asterisk.com>
2021-11-01 13:01:55 +0700

    Chapter 13: User Microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
  get "/login", to: "sessions#new"
  post "/login", to: "sessions#create"
  delete "/logout", to: "sessions#destroy"
@@ -6,7 +8,11 @@ Rails.application.routes.draw do
  get "/signup", to: "users#new"
  post "/signup", to: "users#create"
  get "/show/:id",to: "users#show"
- resources :users, only: %i(new create show)
+ get "/edit/:id", to: "users#edit"
+ resources :users, only: %i(new create show edit update index destroy)
+ resources :account_activations, only: :edit
  get "/static_pages/home", to:"static_pages#home", as: :home
  get "/static_pages/help", to:"static_pages#help", as: :help
+ resources :password_resets, only: [:new, :create, :edit, :update]
+ resources :microposts, only: [:create, :destroy]
 end
