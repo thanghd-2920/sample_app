1d16cea3c67943df6ff774b6db1785ed589c813c
thanghd-1196 <ho.duc.thang@sun-asterisk.com>
2021-11-01 13:01:55 +0700

    Chapter 6:Modeling user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
-    get"/static_pages/home", to:"static_pages#home",as: :home
-    get "/static_pages/help",to:"static_pages#help",as: :help
+  get "/static_pages/home", to:"static_pages#home", as: :home
+  get "/static_pages/help", to:"static_pages#help", as: :help
 end
